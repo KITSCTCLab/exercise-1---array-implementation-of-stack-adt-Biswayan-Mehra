821037ed8c56718788c5d8f5810f1e670f22ea81
BISWAYAN MEHRA <108832092+Biswayan-Mehra@users.noreply.github.com>
2022-07-25 19:12:06 +0530

    Stack_implementation_URK21CS1004

--- Main.py
+++ Main.py
@@ -24,7 +24,6 @@ class Stack:
         for element in self.items:
             print(element)
 
-# Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
 for line in range(queries):
